13f05f27774f00e41d420d3749c4b8d15ac6582d
yashrajvm <yashrajvumolawade@gmail.com>
2020-07-28 20:01:08 +0530

    Adde basic templates of navbar, changed tweet format

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,22 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello world!</h1>")
+    # return HttpResponse("<h1>Hello world!</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 128)} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweet_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
